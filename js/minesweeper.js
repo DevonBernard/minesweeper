7449db10032890cdaa300c365990e0f1fd8913cc
Devon Bernard <dwbcoding@gmail.com>
2015-12-16 19:34:14 -0500

    Added main methods for board setup

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -6,59 +6,6 @@ var Minesweeper = function(ele)
     this.board = [];
 };
 
-Minesweeper.prototype.start = function()
-{
-    this.createBoard();
-    this.setMines();
-};
-
-Minesweeper.prototype.createBoard = function()
-{
-    for(var y = 0; y < this.dimensions.y; y++)
-    {
-        var row = [];
-        for(var x = 0; x < this.dimensions.x; x++)
-        {
-            row.push(new Tile());
-        }
-        this.board.push(row);
-    }
-};
-
-Minesweeper.prototype.setMines = function()
-{
-    var minesSet = 0;
-    while(minesSet < this.mines)
-    {
-        var x = Math.floor((Math.random() * this.dimensions.x));
-        var y = Math.floor((Math.random() * this.dimensions.y));
-
-        if(this.board[y][x].isMine === false)
-        {
-            this.setMine(x,y);
-            minesSet++;
-        }
-    }
-};
-
-Minesweeper.prototype.setMine = function(x, y)
-{
-    this.board[y][x].isMine = true;
-    for(var y2 = y-1; y2 <= y+1; y2++)
-    {
-        if(y2 >= 0 && y2 < this.dimensions.y)
-        {
-            for(var x2 = x-1; x2 <= x+1; x2++)
-            {
-                if(x2 >= 0 && x2 < this.dimensions.y && !(x2 === x && y2 === y))
-                {
-                    this.board[y2][x2].count++;
-                }
-            }
-        }
-    }
-};
-
 var Tile = function()
 {
     this.opened = false;
