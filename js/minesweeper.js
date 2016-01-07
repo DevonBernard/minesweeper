84849d58e47d971801986047156597edfea1337c
Devon Bernard <dwbcoding@gmail.com>
2016-01-07 03:38:30 -0500

    Implemented functionality for validation and auto fill

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,24 +1,123 @@
-var Minesweeper = function(ele)
+function Minesweeper(ele, settings)
 {
+    // Settings
     this.container = $(ele);
-    this.dimensions = {x: 8, y:8};
-    this.mines = 10;
+    this.settings = settings || {};
+    this.settings.dimensions = this.settings.dimensions || {x:8, y:8};
+    this.settings.numMines = this.settings.numMines || 10;
+
+    // Status
     this.flags = 0;
     this.tilesOpened = 0;
-    this.board = [];
     this.cheatModeOn = false;
     this.gameBoard = $("<div class='board mask'></div>");
 };
 
-Minesweeper.prototype.start = function()
+function Grid(width, height, numMines)
 {
+    this.dimensions = {x:width, y:height};
+    this.numMines = numMines;
+    this.numFlags = 0;
+    this.numOpenTiles = 0;
+    this.tiles = [];
+};
+
+Minesweeper.prototype.Start = function()
+{
+    this.grid = new Grid(this.settings.dimensions.x, this.settings.dimensions.y, this.settings.numMines);
     this.container.addClass('minesweeper');
-    this.createBoard();
-    this.setMines();
+    this.grid.createBoard();
+    this.grid.setMines();
 
     this.renderHeader();
     this.renderBoard();
     this.renderFooter();
+    $(this.gameBoard).click((function(e){
+        var target = e.target;
+        var isTile = $(target).hasClass('tile');
+
+        if(!isTile)
+        {
+            target = target.parentElement;
+            isTile = $(target).hasClass('tile');
+        }
+
+        if(isTile)
+        {
+            var x = $(target).data('x');
+            var y = $(target).data('y');
+
+            if(this.grid.tiles[y][x].count == 0)
+            {
+                var tileList = [[x,y]];
+
+                while(tileList.length != 0)
+                {
+                    var tX = tileList[0][0];
+                    var tY = tileList[0][1];
+                    if(tY < this.grid.dimensions.y && tY >= 0 && tX >= 0 && tX < this.grid.dimensions.x)
+                    {
+                        if(this.grid.tiles[tY][tX].flagged === true)
+                            this.grid.numFlags -= 1;
+
+                        var res = this.grid.tiles[tY][tX].open();
+
+                        if(res === 0)
+                        {
+                            this.grid.numOpenTiles++;
+                            tileList.push([tX-1,tY-1]);
+                            tileList.push([tX,tY-1]);
+                            tileList.push([tX+1,tY-1]);
+                            tileList.push([tX-1,tY]);
+                            tileList.push([tX+1,tY]);
+                            tileList.push([tX-1,tY+1]);
+                            tileList.push([tX,tY+1]);
+                            tileList.push([tX+1,tY+1]);
+                        }
+                        else if(res != false)
+                        {
+                            this.grid.numOpenTiles++;
+                        }
+                    }
+                    tileList.shift();
+                }
+            }
+            else
+            {
+                if(this.grid.tiles[y][x].flagged === true)
+                            this.grid.numFlags -= 1;
+                this.grid.tiles[y][x].open();
+                this.grid.numOpenTiles++;
+            }
+
+            this.updateHeader();
+        }
+    }).bind(this));
+
+    $(this.gameBoard).bind("contextmenu",(function(e){
+        e.preventDefault();
+
+        var target = e.target;
+        var isTile = $(target).hasClass('tile');
+
+        if(!isTile)
+        {
+            target = target.parentElement;
+            isTile = $(target).hasClass('tile');
+        }
+
+        if(isTile)
+        {
+            var x = $(target).data('x');
+            var y = $(target).data('y');
+            var tf = this.grid.tiles[y][x].toggleFlag();
+            this.grid.numFlags += tf;
+            this.updateHeader();
+        }
+        
+    }).bind(this));
+
+
 };
 
 Minesweeper.prototype.restart = function()
@@ -28,11 +127,22 @@ Minesweeper.prototype.restart = function()
 
     $(this.container).empty();
     $(this.gameBoard).empty();
-    this.board = [];
-    this.start();
+    this.Start();
 };
 
-Minesweeper.prototype.createBoard = function()
+Minesweeper.prototype.validate = function()
+{
+    if(this.grid.numOpenTiles == ((this.settings.dimensions.x * this.settings.dimensions.y) - this.settings.numMines))
+    {
+        alert("WIN");
+    }
+    else
+    {
+        alert("LOSE");
+    }
+};
+
+Grid.prototype.createBoard = function()
 {
     for(var y = 0; y < this.dimensions.y; y++)
     {
@@ -41,19 +151,25 @@ Minesweeper.prototype.createBoard = function()
         {
             row.push(new Tile());
         }
-        this.board.push(row);
+        this.tiles.push(row);
     }
 };
 
-Minesweeper.prototype.setMines = function()
-{
+Grid.prototype.setMines = function()
+{   
+    if(this.numMines >= this.dimensions.x * this.dimensions.y)
+    {
+        alert("Too many mines");
+        return;
+    }
+
     var minesSet = 0;
-    while(minesSet < this.mines)
+    while(minesSet < this.numMines)
     {
         var x = Math.floor((Math.random() * this.dimensions.x));
         var y = Math.floor((Math.random() * this.dimensions.y));
 
-        if(this.board[y][x].isMine === false)
+        if(this.tiles[y][x].isMine === false)
         {
             this.setMine(x,y);
             minesSet++;
@@ -61,38 +177,38 @@ Minesweeper.prototype.setMines = function()
     }
 };
 
-Minesweeper.prototype.setMine = function(x, y)
+Grid.prototype.setMine = function(x, y)
 {
-    this.board[y][x].isMine = true;
+    this.tiles[y][x].isMine = true;
     for(var y2 = y-1; y2 <= y+1; y2++)
     {
         if(y2 >= 0 && y2 < this.dimensions.y)
         {
             for(var x2 = x-1; x2 <= x+1; x2++)
             {
-                if(x2 >= 0 && x2 < this.dimensions.y && !(x2 === x && y2 === y))
+                if(x2 >= 0 && x2 < this.dimensions.x && !(x2 === x && y2 === y))
                 {
-                    this.board[y2][x2].count++;
+                    this.tiles[y2][x2].count++;
                 }
             }
         }
     }
 };
 
-Minesweeper.prototype.logBoard = function()
+Grid.prototype.logBoard = function()
 {
     for(var y = 0; y < this.dimensions.y; y++)
     {
         var output = "";
         for(var x = 0; x < this.dimensions.x; x++)
         {
-            if(this.board[y][x].isMine)
+            if(this.tiles[y][x].isMine)
             {
                 output += "x ";
             }
             else
             {
-                output += this.board[y][x].count + " ";
+                output += this.tiles[y][x].count + " ";
             }
         }
         console.log(output);
@@ -102,12 +218,12 @@ Minesweeper.prototype.logBoard = function()
 Minesweeper.prototype.renderBoard = function()
 {
     var boardContainer = $("<div class='board-container row'>");
-    for(var y = 0; y < this.dimensions.y; y++)
+    for(var y = 0; y < this.grid.dimensions.y; y++)
     {
         var row = $("<div class='board-row' />");
-        for(var x = 0; x < this.dimensions.x; x++)
+        for(var x = 0; x < this.grid.dimensions.x; x++)
         {
-            row.append(this.board[y][x].render());
+            row.append(this.grid.tiles[y][x].render(x, y));
         }
         this.gameBoard.append(row);
     }
@@ -124,6 +240,10 @@ Minesweeper.prototype.renderFooter = function()
     var button1 = $("<div class='ms-btn'>Validate</div>");
     var button2 = $("<div class='ms-btn' id='ms-btn-reset'>Reset</div>");
     var button3 = $("<div class='ms-btn' id='ms-btn-cheat'>Cheat (OFF)</div>");
+
+    $(button1).click((function(){
+        this.validate();
+    }).bind(this));
 
     $(button2).click((function(){
         this.restart();
@@ -157,10 +277,10 @@ Minesweeper.prototype.renderHeader = function()
 };
 
 Minesweeper.prototype.updateHeader = function(){
-    $('#ms-grid').text(this.dimensions.x + " x " + this.dimensions.y);
-    $("#ms-mines").text(this.mines);
-    $("#ms-flags").text(this.flags);
-    $("#ms-tiles").text(this.tilesOpened + " / " + ((this.dimensions.x * this.dimensions.y) - this.mines));
+    $('#ms-grid').text(this.settings.dimensions.x + " x " + this.settings.dimensions.y);
+    $("#ms-mines").text(this.settings.numMines);
+    $("#ms-flags").text(this.grid.numFlags);
+    $("#ms-tiles").text(this.grid.numOpenTiles + " / " + ((this.settings.dimensions.x * this.settings.dimensions.y) - this.settings.numMines));
 };
 
 Minesweeper.prototype.toggleCheat = function(){
@@ -184,11 +304,11 @@ var Tile = function()
     this.count = 0;
 };
 
-Tile.prototype.render = function()
+Tile.prototype.render = function(x, y)
 {
     var value;
 
-    this.div = $("<div class='tile'></div>");
+    this.div = $("<div class='tile' data-x='"+x+"' data-y='"+y+"'></div>");
 
     if(this.isMine)
         this.div.append("<i class='fa fa-bomb'></i>");
@@ -197,20 +317,16 @@ Tile.prototype.render = function()
     else
         this.div.append("<span></span>");
 
-    $(this.div).click((function(){
-        this.open();
-    }).bind(this));
-
-    $(this.div).bind("contextmenu",(function(e){
-        e.preventDefault();
-        this.toggleFlag();
-    }).bind(this));
-
     return this.div;
 };
 
 Tile.prototype.open = function()
 {
+    if(this.opened == true)
+    {
+        return false;
+    }
+
     if(this.flagged)
     {
         this.toggleFlag();
@@ -227,6 +343,7 @@ Tile.prototype.open = function()
     {
         $(this.div).addClass('box'+this.count)
     }
+    return this.count;
 };
 
 Tile.prototype.toggleFlag = function()
@@ -244,5 +361,7 @@ Tile.prototype.toggleFlag = function()
 
         this.flagged = !this.flagged;
         $(this.div).toggleClass("flag");
+        return this.flagged ? 1 : -1;
     }
+    return 0;
 };
